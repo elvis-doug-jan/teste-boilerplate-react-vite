02e139607744055b1c924e123e7a101d27fa8f4a
Elvis Douglas Janegitz <edjelvis@gmail.com>
2023-04-23 19:43:25 -0300

    Initial commit

---
+++ test/components/navigations/SwitchNavigator.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type {RenderResult} from '@testing-library/react';
+import RootProvider from '../../../src/providers/index';
+import SwitchNavigator from '../../../src/components/navigations/SwitchNavigator';
+import {render} from '@testing-library/react';
+
+const props = {};
+
+const component = (
+  <RootProvider initialThemeType="light">
+    <SwitchNavigator {...props} />
+  </RootProvider>
+);
+
+describe('[SwitchNavigator] rendering', () => {
+  let testingLib: RenderResult;
+
+  beforeEach(() => {
+    testingLib = render(component);
+  });
+
+  it('should renders without crashing', () => {
+    const {baseElement} = testingLib;
+
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ test/components/uis/UserCard.test.tsx
@@ -0,0 +1,16 @@
+import * as renderer from 'react-test-renderer';
+
+import React from 'react';
+import UserCard from '../../../src/components/uis/UserCard';
+import {createTestElement} from '../../utils/testUtils';
+
+const props = {};
+const component = createTestElement(<UserCard {...props} />);
+
+describe('[Temp] render', () => {
+  it('renders without crashing', () => {
+    const rendered = renderer.create(component).toJSON();
+
+    expect(rendered).toBeTruthy();
+  });
+});
